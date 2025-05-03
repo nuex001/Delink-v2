3edc49b7213d927a02f745f307b13b506b1c9341
nuex001 <edeemmanuelchizurumoke@gmail.com>
2025-05-03 07:39:11 +0100

    Worked on adding the our guild link on the home page

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,10 +1,13 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import React, { useEffect, useRef, useState } from "react";
 import { FaLink } from "react-icons/fa6";
+import { useLocation } from "react-router-dom"; // STEP 1
+
 function Navbar() {
   const [copied, setcopied] = useState(false);
+  const [isHomePage, setIsHomePage] = useState(false);
   const timeoutRef = useRef(null); // store the timeout
-
+  const pathname = useLocation().pathname; // STEP 2
   const copyLink = (e) => {
     e.preventDefault(); // optional, if it's inside a button or link
     navigator.clipboard
@@ -24,6 +27,9 @@ function Navbar() {
   };
 
   useEffect(() => {
+    setIsHomePage(pathname === "/"); // STEP 3
+  }, [pathname]);
+  useEffect(() => {
     return () => {
       if (timeoutRef.current) {
         clearTimeout(timeoutRef.current);
@@ -33,13 +39,19 @@ function Navbar() {
 
   return (
     <nav>
-      <a href="#" className="logo">
+      <a href="/" className="logo">
         DeLink
       </a>
-      <button onClick={copyLink}>
-       <FaLink className="icon"/>
-        {copied ? " Copied" : "My Link"}
-      </button>
+      {!isHomePage ? (
+        <button onClick={copyLink} className="btn">
+          <FaLink className="icon" />
+          {copied ? " Copied" : "My Link"}
+        </button>
+      ) : (
+        <a href="/" target="_blank" className="btn">
+          Your Guild
+        </a>
+      )}
     </nav>
   );
 }
