052850b699414b056fc0b8e14f111e623ab6a843
nuex001 <edeemmanuelchizurumoke@gmail.com>
2025-09-05 22:05:27 +0000

    changed the buy me coffee to only show on the base names

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -164,7 +164,6 @@ function Home() {
             {bnsDomainState && !bnsDomainState.endsWith(".base.eth") ? (
               <></>
             ) : (
-             
               <div className="pay_container" ref={buyCoffeeRef}>
                 <h1>Buy {bnsDomainState} a coffee</h1>
                 <div className="payForm">
@@ -186,26 +185,28 @@ function Home() {
               </div>
             )}
           </div>
-          <div className="payment_btn" onClick={toggleBuyCoffee}>
-            <span>Buy Coffee</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="lucide lucide-coffee-icon lucide-coffee"
-            >
-              <path d="M10 2v2" />
-              <path d="M14 2v2" />
-              <path d="M16 8a1 1 0 0 1 1 1v8a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4V9a1 1 0 0 1 1-1h14a4 4 0 1 1 0 8h-1" />
-              <path d="M6 2v2" />
-            </svg>
-          </div>
+          {bnsDomainState && bnsDomainState.endsWith(".base.eth") && (
+            <div className="payment_btn" onClick={toggleBuyCoffee}>
+              <span>Buy Coffee</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="lucide lucide-coffee-icon lucide-coffee"
+              >
+                <path d="M10 2v2" />
+                <path d="M14 2v2" />
+                <path d="M16 8a1 1 0 0 1 1 1v8a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4V9a1 1 0 0 1 1-1h14a4 4 0 1 1 0 8h-1" />
+                <path d="M6 2v2" />
+              </svg>
+            </div>
+          )}
           <div className="rows">
             {/* Avatar */}
             <img
